91930db1db6d61da569733ca77de69adf4d7a9f3
vagfou <vagfou@protonmail.com>
2024-09-19 05:28:15 +0300

    javascripting done?

---
+++ javascripting/array-filtering.js
@@ -0,0 +1,9 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10]
+
+// let filtered = numbers.filter(function (number) {
+//     return (number % 2 === 0)
+// })
+
+let filtered = numbers.filter(number => number % 2 === 0 )
+
+console.log(filtered)
---
+++ javascripting/function-arguments.js
@@ -0,0 +1,3 @@
+let math = (a,b,c) => ((c * b) + a)
+
+console.log(math(53,61,67))
